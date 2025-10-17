c3dc1f98318263813190887bd5f7556aa35b8c56
Jhonven <jhonvendelosreyes836@gmail.com>
2025-10-17 13:13:59 +0800

    Delete 3star directory

--- 3star/update_admin_password.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'config.php';
-$newHash = password_hash('admin123', PASSWORD_DEFAULT);
-mysqli_query($conn, "UPDATE admins SET password='$newHash' WHERE username='admin'");
-echo "Admin password updated!";
-?>
